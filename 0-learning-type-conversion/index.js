1b40222023ef0ceb5c15878b3728cba3421d9e12
Joed <joeddonaire@gmail.com>
2026-03-09 13:51:39 +0800

    Learned type conversiond and block scopes with let and const

---
+++ 0-learning-type-conversion/index.js
@@ -0,0 +1,41 @@
+// CONVERTING STRING TO A NUMBER  
+{
+    let x = "20"; 
+    let y = "10" ;
+    
+    x = Number(x) ;
+    y = Number(y) ; 
+
+    // or alternatively 
+    // x = + x ; 
+    // y = + y ;
+    
+    let z = x + y ; 
+    
+    console.log("--------------") ;
+    console.log(x) ;
+    console.log(typeof(x) ) ; 
+    console.log(y) ;
+    console.log(typeof(y) ) ; 
+    console.log("--------------") ;
+}
+
+// CONVERTING NUMBER TO A STRING
+{
+    let x = 20 ; 
+    let y = 10 ; 
+    
+    x = String(x) ;
+    y = String(y) ;
+
+    // or alternatively
+    x = x + "" 
+    y = y + ""
+    
+    console.log("--------------") ;
+    console.log(x, y, typeof(x), typeof(y)) ;
+    console.log("--------------") ;
+
+
+
+}
