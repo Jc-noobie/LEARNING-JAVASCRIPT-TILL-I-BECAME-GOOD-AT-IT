9adeedfc33c36c64ff69ef7cb7d5c0ab92f4afcc
Joed <joeddonaire@gmail.com>
2026-03-09 13:53:45 +0800

    Learned static math functions

---
+++ 1-learning-math-functions/index.js
@@ -0,0 +1,37 @@
+// learning Math static functions
+
+let x = 100 ;
+let y = 250 ;
+let z = 50 ;
+
+
+let a = 2.99 ; 
+let b = 4.99 ;
+let c = 3.1 ;
+
+// CONSTANTS
+console.log(Math.PI) ;
+console.log("------")
+
+// ROUNDING 
+console.log(Math.ceil(a)) ;
+console.log(Math.floor(a)) ;        
+console.log("------")
+
+// ALGEBRA 
+console.log(Math.sqrt(x)) ;
+console.log(Math.pow(x ,2 ) ) ;
+console.log(Math.abs(c)) ;
+console.log("------")
+
+// COMPARISON
+console.log(Math.max(x, y)) ;
+console.log(Math.min(x, y)) ;
+console.log(Math.max(x,y,z) ) ;
+
+console.log("------")
+// TRIGONOMETRY 
+console.log(Math.sin(x) ) ;
+console.log(Math.cos(y) ) ;
+console.log(Math.tan(z) ) ;
+
