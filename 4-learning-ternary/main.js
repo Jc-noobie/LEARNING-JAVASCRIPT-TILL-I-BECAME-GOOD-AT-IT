e4d79a86bb8dd93dca1e359788639cd41545def2
Joed <joeddonaire@gmail.com>
2026-03-11 23:38:52 +0800

    Learned how ternary operators work

---
+++ 4-learning-ternary/main.js
@@ -0,0 +1,8 @@
+let username = "joed" ;
+let password = "joed123" ;
+
+let nameChecker =  username === "joed" ?  true  : false ;
+let passwordChecker = password === "joed123" ? true : false ; 
+
+console.log(nameChecker) ;
+console.log(passwordChecker) ;
