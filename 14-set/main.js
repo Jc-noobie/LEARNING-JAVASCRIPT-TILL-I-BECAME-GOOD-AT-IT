22245ad64353d5938e3e413cf31ca949dc863cdb
Joed <joeddonaire@gmail.com>
2026-05-11 00:18:40 +0800

    learned set

--- 14-set/main.js
+++ 14-set/main.js
@@ -2,6 +2,11 @@ const letters = new Set() ;
 
 letters.add('a') ;
 letters.add('b') ;
+letters.add('c') ;
+letters.add('d') ;
+letters.add('e') ;
+letters.add('f') ;
 
 console.log(letters) ; 
+console.log(letters.has('g')) ;
 
